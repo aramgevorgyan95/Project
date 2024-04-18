1bb078b11cba4a04082f3bedb2db82df6672efba
aramgevorgyan95 <aramgevorgyan9599@gmail.com>
2024-04-18 21:13:57 +0300

    addNewCommit

--- src/components/translate_component/translate.js
+++ src/components/translate_component/translate.js
@@ -1,19 +1,21 @@
 import { useState } from 'react';
 import './style.css';
-import React, { useContext } from 'react';
-import TranslateContext from '../../translateContext';
+import React from 'react';
+// import { useContext } from 'react';
+import { useTranslation } from 'react-i18next';
+// import TranslateContext from '../../translateContext';
 
 
 
 
 function Translate({ language1, language2 }) {
 
-    const t = useContext(TranslateContext)
-
+    // const t = useContext(TranslateContext)
+    const { t, i18n } = useTranslation();
     const [show, isShow] = useState(true);
 
     const handleClickTranslate = function (e) {
-        t.setTranslatePage(e.target.innerHTML)
+        i18n.changeLanguage(e)
         isShow(prev => !prev)
     }
 
@@ -22,8 +24,8 @@ function Translate({ language1, language2 }) {
         <div>
 
             <div className="container2">
-                <button onClick={handleClickTranslate} className={show ? 'button_translate_avtive' : 'button_translate_noactive'}>{language1}</button>
-                <button onClick={handleClickTranslate} className={show ? 'button_translate_noactive' : 'button_translate_avtive'}>{language2}</button>
+                <button onClick={() => handleClickTranslate('RU')} className={show ? 'button_translate_avtive' : 'button_translate_noactive'}>{language1}</button>
+                <button onClick={() => handleClickTranslate('EN')} className={show ? 'button_translate_noactive' : 'button_translate_avtive'}>{language2}</button>
             </div>
 
         </div>
