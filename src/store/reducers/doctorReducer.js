32a71e50062d647aae0298f935e988ef3d143d55
aramgevorgyan95 <aramgevorgyan9599@gmail.com>
2024-04-29 21:07:45 +0400

    SAGA?

--- src/store/reducers/doctorReducer.js
+++ src/store/reducers/doctorReducer.js
@@ -8,7 +8,6 @@ const initData = {
 
 export default function (state = initData, action) {
     const { type, payload } = action;
-    console.log(action.payload.id);
     switch (type) {
         case DoctorType.SET_DOCTORS_LIST:
             return { ...state, doctorList: payload }
